fc553b9f7b25816a5111c8f8f76b4373df529405
Aaron Bujatin <aaronbujatin@gmail.com>
2023-07-22 11:28:39 +0800

    Add files via upload

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../model/user.model';
+import { environment } from 'src/environments/environment.development';
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +11,11 @@ export class UserService {
 
   constructor(private httpClient: HttpClient) { }
 
-  private readonly API_URL = "http://localhost:8080/api/v1/users";
+  private API_URL = environment.baseUrl;           
 
 
-  public sendInquiry(user:User) {
-    return this.httpClient.post(this.API_URL, user);
+  public sendInquiry(user: User) {
+    return this.httpClient.post<any>(`${this.API_URL}/api/v1/users`, user);
   }
 
 }
