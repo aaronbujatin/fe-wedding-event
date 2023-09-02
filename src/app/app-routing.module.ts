a4f1e2d536fe3aa615b4e0bb36d2952fea0fa1d4
Aaron Bujatin <aaronbujatin@gmail.com>
2023-09-02 17:20:49 +0800

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,18 +5,24 @@ import { ContactComponent } from './components/contact/contact.component';
 import { AboutComponent } from './components/about/about.component';
 import { GalleryComponent } from './components/gallery/gallery.component';
 import { PackageOffersComponent } from './components/package-offers/package-offers.component';
+import { DiamondComponent } from './components/packages/diamond/diamond.component';
+import { EmeraldComponent } from './components/packages/emerald/emerald.component';
+import { GoldComponent } from './components/packages/gold/gold.component';
 
 const routes: Routes = [
-  {path : '', component : HomeComponent},
-  {path: 'contact', component: ContactComponent},
-  {path: 'about', component: AboutComponent},
-  {path: 'gallery', component: GalleryComponent},
-  {path: 'package-offers', component: PackageOffersComponent}
+  { path: '', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'package-offers', component: PackageOffersComponent },
+  { path: 'package-gold', component: GoldComponent },
+  { path: 'package-emerald', component: EmeraldComponent },
+  { path: 'package-diamond', component: DiamondComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  
-exports: [RouterModule]
+
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
